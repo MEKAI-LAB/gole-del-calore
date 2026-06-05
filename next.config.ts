146c7c706468fbd29456ead347e4e8da52edc0f8
2Effs <fabiofesta0807@gmail.com>
2026-06-06 01:55:00 +0200

    Redirect Vercel domain to canonical domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "gole-del-calore.vercel.app",
+          },
+        ],
+        destination: "https://legoledelcalore.it/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
